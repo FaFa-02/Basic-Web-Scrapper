4e0475b8de0c270190e19ee86553c8bce7f88ddb
FaFa-02 <fabioeugenio.doria@gmail.com>
2024-06-04 20:09:34 +0100

    feat: requests and parses target web page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Requests web page and parses into soup
 target_page = requests.get("https://quotes.toscrape.com/")   
+soup = BeautifulSoup(target_page.text, "html.parser")
