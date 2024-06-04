c16b304056dd50b2abd1bb5fee48d3e7f4d83a55
FaFa-02 <fabioeugenio.doria@gmail.com>
2024-06-04 20:31:51 +0100

    feat: finds and stores all quotes and authors in seperate lists

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import requests
 # Requests web page and parses into soup
 target_page = requests.get("https://quotes.toscrape.com/")   
 soup = BeautifulSoup(target_page.text, "html.parser")
+
+# Find all quotes and authors in the html
+quotes = soup.find_all("span", attrs={"class":"text"})
+authors = soup.find_all("small", attrs={"class":"author"})
+
+# Store all quotes in a list
+quote_arr = []
+for quote in quotes:
+    quote_arr.append(quote.string)
+
+# Store all authors in a list
+author_arr = []
+for author in authors:
+    author_arr.append(author.string)
